bff4e3d74f6f54b7fcd06b06073e2c19814ef3e1
Timothy Onoja <timothyonoja1@gmail.com>
2023-07-25 09:56:51 -0700

    second commit

---
+++ app/src/main/java/com/vtopacademy/ApplicationClass.kt
@@ -0,0 +1,8 @@
+package com.vtopacademy
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/** Application entry point. Configures dependency injection. */
+@HiltAndroidApp
+class ApplicationClass : Application()
